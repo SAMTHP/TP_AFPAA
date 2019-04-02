4173102ad1743b4261aa90aa6e859b9bd1e1f6df
SAMTHP <samir_615@live.fr>
2019-04-02 17:20:47 +0200

    MAJ

---
+++ MAQUETTE_WEB_SITE_01/app/view/footer.php
@@ -0,0 +1,4 @@
+
+    <a href="#">MENTIONS LEGALES</a>
+    <a href="#">PLAN DU SITE</a>
+    <a href="#">CONTACT</a>
---
+++ MAQUETTE_WEB_SITE_01/app/view/nav.php
@@ -0,0 +1,24 @@
+
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="#" width="120px">ACCUEIL</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" width="120px">PRESENTATION</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" width="120px">CONTEXTES D'INTERVENTIONS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" width="120px">AUDIT & DIAGNOSTIC STRATEGIQUES</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" width="120px">ACCOMPAGNEMENT OPERATIONNEL</a>
+        </li>
+        <li class="nav-item" width="80px">
+            <a class="nav-link" href="#" width="120px">DIRECTION FINANCIERE EXTERNALISEE</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" >CONTACT & EQUIPE</a>
+        </li>
+    </ul>
